9242517cf9e1570d11e50bc2f862c88605a9523b
Nelson Sousa <nelson.sousa.it@gmail.com>
2025-06-04 16:29:42 +0100

    fix: replace list[str] with List[str] for Python <3.9 compatibility

--- interview/inventory/schemas.py
+++ interview/inventory/schemas.py
@@ -1,10 +1,11 @@
 
 from decimal import Decimal
 from pydantic import BaseModel
+from typing import List
 
 
 class InventoryMetaData(BaseModel):
     year: int
-    actors: list[str]
+    actors: List[str]
     imdb_rating: Decimal
     rotten_tomatoes_rating: int
